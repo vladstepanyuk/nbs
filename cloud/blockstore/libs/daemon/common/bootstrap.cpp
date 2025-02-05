586d7178900af66183c8139a59182e3fa8b199fe
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-02-05 09:39:01 +0000

    issue-2947: correct issue

--- cloud/blockstore/libs/daemon/common/bootstrap.cpp
+++ cloud/blockstore/libs/daemon/common/bootstrap.cpp
@@ -898,7 +898,7 @@ void TBootstrapBase::Start()
     START_COMMON_COMPONENT(Scheduler);
 
     if (!Configs->Options->TemporaryServer) {
-        Warmup();
+        WarmupBSGroupsConnections();
     }
 
     auto restoreFuture = EndpointManager->RestoreEndpoints();
